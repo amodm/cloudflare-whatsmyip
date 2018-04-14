e538fbfa45a0242de1907497dd40dccb418bcae2
Amod Malviya <amod@rootnet.in>
2018-04-14 18:20:33 +0530

    Set cache-control to no-cache

--- worker.js
+++ worker.js
@@ -5,9 +5,10 @@ addEventListener('fetch', event => {
 
   if (req.url.endsWith('/json')) {
     const content = JSON.stringify({ ip, country }, null, 2);
-    const headers = new Headers({ "Content-Type": "application/json;charset=UTF-8" });
+    const headers = new Headers({ 'Content-Type': 'application/json;charset=UTF-8', 'Cache-Control': 'no-cache' });
     event.respondWith(new Response(`${content}\n`, { headers }));
   } else {
+    const headers = new Headers({ 'Content-Type': 'text/plain;charset=UTF-8', 'Cache-Control': 'no-cache' });
     event.respondWith(new Response(`${ip}\n`));
   }
 })
